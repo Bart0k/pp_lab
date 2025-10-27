90f24fcbb1b03f9d989322f30155d1adc5d3b2c8
Bart0k <bartosz.bbednarek@gmail.com>
2025-10-27 16:12:14 +0100

    zamiany v1

---
+++ Projekt_petle/Projekt_petle/Projekt_petle.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+//int main()
+//{
+//    for (int i = 10; i <= 10; i++) {
+//        cout << i << endl;
+//        i = i++;
+//    }
+//    return 0;
+//}
+
+//int main() {
+//   int fahr, celsius; 
+//    int start, limit, krok; 
+//    start = 0;
+//    cout << "Enter the limit: ";
+//   cin >> limit; 
+//    cout << "Enter the step: ";
+//    cin >> krok;
+//    for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)  
+//    {
+//        float celsius = (5.0 / 9.0) * (fahr - 32.0);
+//     cout << fahr << "\t" << celsius << endl;
+//  }
+
+//	}
+//zapomnialem i przez przypadek zamiast czy dodatnia sprawdza czy parzysta
+
+//int main() {
+//	int n;
+//	for (int i = 0; i < 10; i++) {
+//		cout << "Give a number: ";
+//		cin >> n;
+//		if (n % 2 == 0) {
+//			cout << n << endl;
+//		}
+//		else {
+//			continue;
+//		}
+//	}
+//
+//}
+
