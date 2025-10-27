603312919873883c50eb9b3339b156141e0dbba9
Bart0k <bartosz.bbednarek@gmail.com>
2025-10-27 17:42:51 +0100

    zadanie 3

---
+++ Lab3/Lab3/zad3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	float r;
+	cout << "Give R:";
+	cin >> r
+		if (0.7 <= r && r <= 1) {
+			cout << "Very strong positive corelation";
+	} 
+}
