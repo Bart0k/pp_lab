f3cb4fde0e8bfca3a212b7a46486d32974f8fd11
Bart0k <bartosz.bbednarek@gmail.com>
2025-10-27 17:49:53 +0100

    zadanie 3.v2

--- Lab3/Lab3/zad3.cpp
+++ Lab3/Lab3/zad3.cpp
@@ -7,5 +7,17 @@ int main() {
 	cin >> r
 		if (0.7 <= r && r <= 1) {
 			cout << "Very strong positive corelation";
-	} 
+		}
+		else if (r >= 0.5 && r < 0.7) {
+			cout << "Strong positive corelation";
+		}
+		else if (r >= 0.3 && r < 0.5) {
+			cout << "Medium positive corelation";
+		}
+		else if (r >= 0.2 && r < 0.3) {
+			cout << "Weak positive corelation";
+		}
+		else {
+			cout << "No corelation";
+		}
 }
