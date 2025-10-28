2f3c1d324a6d913c8775c7e4e4c8c966df0b06cf
Bart0k <bartosz.bbednarek@gmail.com>
2025-10-28 14:38:20 +0100

    Lab4 - funkcje

---
+++ Lab4/Lab4/Lab4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void great(int a, int b);
+
+int main()
+{
+    int a, b;
+    cout << "Enter number A: ";
+    cin >> a;
+    cout << "Enter number B: ";
+    cin >> b;
+    great(a, b);
+
+}
+void great(int a, int b) {
+    //int c;
+    if (a > b) {
+        cout << "A is greater than B";
+    }
+    else if (a < b) {
+        cout << "B is greater than A";
+    }
+    else {
+        cout << "Numbers are equal";
+    }
+}
