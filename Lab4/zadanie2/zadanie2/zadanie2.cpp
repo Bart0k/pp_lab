aa5ae2ffba7dd867a72d2a68f992f174efe966f4
Bart0k <bartosz.bbednarek@gmail.com>
2025-10-28 14:48:56 +0100

    zadanie 2

---
+++ Lab4/zadanie2/zadanie2/zadanie2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void check(int n);
+
+int main()
+{
+    int n;
+    cout << "Enter number N: ";
+    cin >> n;
+    check(n);
+}
+void check(int n) {
+    for (int i = 1; i <= 100; i++) {
+        if (n % i == 0) {
+            cout << i << endl;
+        }
+        else {
+            continue;
+        }
+    }
+}
