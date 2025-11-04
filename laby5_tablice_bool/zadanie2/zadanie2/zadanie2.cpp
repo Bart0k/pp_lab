32672aefbfd8d5b0ca399c8cffb8cc654169620c
Bart0k <bartosz.bbednarek@gmail.com>
2025-11-04 14:18:06 +0100

    zadanie 2

---
+++ laby5_tablice_bool/zadanie2/zadanie2/zadanie2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number n: ";
+    cin >> n;
+    int* tab = new int[n];
+    tab[0] = 0;
+    tab[1] = 1;
+    cout << tab[0] << endl << tab[1] << endl;
+    for (int i = 2; i < n; i++) { 
+        
+        tab[i] = tab[i - 1] + tab[i - 2];
+        cout << tab[i] << endl;
+
+    }
+}
+
