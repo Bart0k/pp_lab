d7fa962d4a58e386ac05b3cec75dbc2fcd492e07
Bart0k <bartosz.bbednarek@gmail.com>
2025-11-04 13:41:43 +0100

    zadanie 1

---
+++ laby5_tablice_bool/laby5_tablice_bool/laby5_tablice_bool.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tab[10];
+
+    cout << "Enter 5 numbers: " << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "\nNumber " << i + 1 << ": ";
+        cin >> tab[i];
+
+    }
+    cout << "Given numbers: \n";
+    for (int i=0; i < 10; i++) {
+        cout << tab[i] << endl; 
+
+    }
+    return 0;
+}
