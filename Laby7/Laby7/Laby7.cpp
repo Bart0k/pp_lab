d18cf88dce6045a2c5a1be8c041fa61cf433e01c
Bart0k <bartosz.bbednarek@gmail.com>
2025-12-02 14:21:26 +0100

    nowe laby

---
+++ Laby7/Laby7/Laby7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int choinka(int n) {
+    if (n == 0) return 0;
+    else {
+        choinka(n - 1);
+        for (int j = -1; j < n; j++) {
+            cout << " ";
+        }
+        for (int i = 0; i < n; i++) {
+            cout << "*";      
+    }
+        cout << endl;
+
+    }
+    
+}
+int main()
+{
+    int n;
+    cout << "Enter numebr n: ";
+    cin >> n;
+    choinka(n);
+    
+}
+
+
---
+++ Laby8/Laby8/Laby8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int n = 0; int m = 0;
+
+int counter(int n, int m) {
+	//counter(n,m,int sum);
+	int  sum = 0;
+	for (int i = 0; i < m; i++) {
+		cout << "Enter an intiger number: ";
+		cin >> n;
+		sum += n;
+	}
+	return sum;
+}
+int main()
+{
+	//int n;
+	cout << "Enter a number: ";
+	cin >> m;
+	int result = counter(n, m);
+	cout << "The total sum of given numbers is: " << result;
+	//counter(n,m);
+	
+
+}
+
