be93e94b894de43afa78fca7e7cd5bb81ed8abe9
Bart0k <bartosz.bbednarek@gmail.com>
2025-12-07 13:12:12 +0100

    zda7

---
+++ Lab3/zadanie7/zadanie7/zadanie7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+
+	bool loop = true;
+	int i = 0;
+	cout << "Enter a number: ";
+	// cin >> a;
+	while (loop && i < 200) {
+		i++;
+		if (i % 2 == 0) {
+			continue;
+		}
+		else {
+			cout << i << endl;
+		}
+	}
+}
