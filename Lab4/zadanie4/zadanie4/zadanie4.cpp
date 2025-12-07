ae4ed160a86ab4dc631537052d81dd59eb84c9ce
Bart0k <bartosz.bbednarek@gmail.com>
2025-12-07 13:44:29 +0100

    zad4

---
+++ Lab4/zadanie4/zadanie4/zadanie4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int result = 1;
+int power(int a, int b, int result) {
+    for (int i = 0; i < b; i++) {
+        result = result * a;
+    }
+    return result;
+}
+int main()
+{
+    int a, b;
+    cout << "Enter the numebr of base: ";
+    cin >> a;
+    cout << "Enter the number of exponent: ";
+    cin >> b;
+    result = power(a, b, result);
+    cout << result;
+}
+
