ff487e4a13acf1302d3eb4c3510b34f25377f4e1
Bart0k <bartosz.bbednarek@gmail.com>
2025-12-07 17:28:34 +0100

    zadanie8

---
+++ Lab4/zadanie8/zadanie8/zadanie8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int even(int n) {
+	return n / 2;
+}
+int odd(int n) {
+	n = n - 1;
+	return n / 2;
+}
+
+int main()
+{
+	int n;
+	for (int i = 0; i <= 100; i++) {
+		n = i;
+		if (n % 2 == 0) {
+			int even_res = even(n);
+			cout << even_res<<endl;
+		}
+		else {
+			int odd_res = odd(n);
+			cout << odd_res << endl;
+		}
+	}
+
+}
