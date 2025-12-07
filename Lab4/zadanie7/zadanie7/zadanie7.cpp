45ab87db4921978b7a0ca3ca9ee39d7a3c400a5e
Bart0k <bartosz.bbednarek@gmail.com>
2025-12-07 17:22:01 +0100

    zadanie7

---
+++ Lab4/zadanie7/zadanie7/zadanie7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int generator(int number, int tab[11]) {
+    int n=0;
+    for (int i = 0; i <= 10; i++) {
+        int random_number = rand() % 10 + 1;
+        tab[i] = random_number;
+        if (random_number == number) {
+            n += 1;
+        }
+        cout << tab[i]<<" ";
+    }
+    return n;
+}
+int main()
+{
+    int number; int tab[11];
+    int sec = time(NULL);
+    srand(sec);
+
+    cout << "Enter a random number from 1 to 10: ";
+    cin >> number;
+    int result = generator(number,tab);
+    cout << "\nYour number " << number << " appeared " << result << " times!";
+}
+
